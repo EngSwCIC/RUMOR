a24b0e0ceaed20b83251c72b1187e056002063a6
Thiago Luis <thiagoluisrpinho@hotmail.com>
2018-10-20 19:45:36 -0300

    Add features with happy and unhappy scenarios

---
+++ features/step_definitions/cardapio/como_gestor_eu_quero_importa_um_cardápio_steps.rb
@@ -0,0 +1,12 @@
+# This is the steps file referred to como_gestor_eu_quero_importa_um_cardápio feature
+# Place your code relative to that feature here
+
+You can implement step definitions for undefined steps with these snippets:
+
+Dado("que eu estou na página de importar cardápio") do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Dado("que eu adiciono o arquivo de {string}") do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
