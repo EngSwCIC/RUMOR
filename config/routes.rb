efdf71fff026c3cb18c002b3d80df46bceeb6c82
Luiz Gustavo Martins <martins.luiz131@gmail.com>
2018-11-07 21:46:18 -0200

    criando tabela menu_reviews e implementando view para menu_review

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :menu_reviews
   get 'rumor/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'rumor#index'
