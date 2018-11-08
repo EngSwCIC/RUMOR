7f43fba5c6fe866405a86de802f4096376cb26b5
Luiz Gustavo Martins <martins.luiz131@gmail.com>
2018-11-08 00:11:30 -0200

    Definicao dos steps para avaliar_cardapio.feature

---
+++ features/step_definitions/avaliacao_steps.rb
@@ -0,0 +1,10 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "../../", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "../../", "support", "selectors"))
+
+Quando /^(?:|Eu )preencho.$/ do |fields|
+  fields.rows_hash.each do |name, value|
+    Quando %{Eu preencho "#{name}" with "#{value}"}
+  end
+end
