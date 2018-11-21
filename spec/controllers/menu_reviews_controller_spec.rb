37780eee66e3f0199dd02e2a306f32664d58062a
Luiz Gustavo Martins <martins.luiz131@gmail.com>
2018-11-21 21:50:07 -0200

    criando rspec para controller menu_reviews

---
+++ spec/controllers/menu_reviews_controller_spec.rb
@@ -0,0 +1,52 @@
+require 'rails_helper'
+
+RSpec.describe MenuReviewsController, type: :controller do
+  let(:valid_attributes) {
+    { nome: 'Luiz',
+      email: 'teste@teste.com',
+      tipo_usuario: 'Aluno',
+      refeicao: 'Jantar',
+      categoria: 'Sopa',
+      nota: '4',
+      consideracao: 'muito boa'
+    }
+  }
+  
+  let(:invalid_attributes) {
+    { nome: '',
+      email: '',
+      tipo_usuario: '',
+      refeicao: '',
+      categoria: '',
+      nota: '',
+      consideracao: ''
+    }
+  }
+
+  describe "POST #create" do
+    context "utilizando entradas válidas" do
+      it "salva a pesquisa do cardápio no banco de dados" do
+        expect{
+          post :create, params: {menu_review: valid_attributes}
+        }.to change(MenuReview, :count).by(1)
+      end
+      it "redireciona para a página inicial do Rumor" do
+        post :create, params: {menu_review: valid_attributes}
+        expect(response).to redirect_to rumor_index_path
+      end
+    end
+    
+    context "utilizando entradas inválidas" do
+      it "não salva a pesquisa do cardápio no banco de dados" do
+        expect{
+          post :create, params: {menu_review: invalid_attributes}
+        }.to_not change(MenuReview, :count)
+      end
+      it "renderiza novamente o formulário para preenchimento" do
+        post :create, params: {menu_review: invalid_attributes}
+        expect(response).to render_template :new
+      end
+    end
+  end
+
+end
