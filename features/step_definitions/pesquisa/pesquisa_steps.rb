1640773cde3d4baac906d1eb349687ab3648918c
VitorRibeiroCustodio <vitorcustodio@cjr.org.br>
2018-11-21 21:26:06 -0200

    Cucumber passando com devise

--- features/step_definitions/pesquisa/pesquisa_steps.rb
+++ features/step_definitions/pesquisa/pesquisa_steps.rb
@@ -7,6 +7,13 @@ Dado("que eu tenha uma lista de pesquisas {string}, {string}, {string}, {string}
 end
 
 Dado("que eu esteja na página index de pesquisas") do
+  email = 'user@user.com'
+  password = 'user123'
+  User.new(:email => email, :password => password, :password_confirmation => password).save!
+  visit new_user_session_path
+  fill_in :Email,with: "user@user.com"
+  fill_in :Password,with: "user123"
+  click_button "Log in"
   visit customer_satisfactions_path
 end
 
