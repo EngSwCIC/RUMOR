90ea62160faaeab1f134241c887525cefa51e4c2
Unknown <thiagoluisrpinho@gmail.com>
2018-12-05 15:18:51 -0200

    Successfully opening the file and reading it

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -17,10 +17,19 @@ class MenusController < ApplicationController
   def edit
   end
 
+# Create method will be called if the user wishes to create a new menu or if he/she chooses
+# to import an already existing valid sheet
   def create
     case route_to params
       when :create_sheet
-        raise "xablau"
+        load_imported_menu(params[:menu][:file])
+        raise params[:menu][:file].original_filename.inspect
+        @menu_import = Menu.new(params[:menu])
+        if @menus_import.save
+          redirect_to menus_path
+        else
+          render :new
+        end
       else
         @menu = Menu.new(menu_params)
           if @menu.save
@@ -64,6 +73,22 @@ class MenusController < ApplicationController
       if params[:route_to]
         params[:route_to].keys.first.to_sym
       end
+    end
+
+    # Private method used to open an external .csv, .xls or .xlsx file 
+    def open_spreadsheet(file)
+      case File.extname(file.original_filename)
+      when ".csv" then Csv.new(file.path, nil, :ignore)
+      when ".xls" then Roo::Excel.new(file.path, nil, :ignore)
+      when ".xlsx" then Roo::Excelx.new(file.path)
+      else raise "Unknown file type: #{file.original_filename}"
+      end
+    end
+
+    # Privated method to load an file and organized it as menu meals
+    def load_imported_menu(file)
+      spreadsheet = open_spreadsheet(file)
+      raise spreadsheet.row(5).inspect
     end
 
 end
