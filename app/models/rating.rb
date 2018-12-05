e6866ebc14e5a5a73ef204f5ebf640491d566e8b
AmanajasLG <luizamanajas@hotmail.com>
2018-12-05 00:24:26 -0200

    Comentáeios funções e Rdoc

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,29 +1,30 @@
 class Rating < ApplicationRecord
 
+    # Retorna uma mensagem de acordo com a nota do cardápio
     def rating_notification
-
-        if self.rating === -1
+        @rating = self.rating
+        if @rating === -1
             return 'Nenhuma avaliação foi feita ainda :('
-        elsif self.rating < 1
+        elsif @rating < 1
             return 'Se eu fosse você comia na lanchonete'
-        elsif self.rating < 2.5
+        elsif @rating < 2.5
             return 'Não recomento mas se quiser arriscar...'
-        elsif self.rating < 4
+        elsif @rating < 4
             return 'Vale o investimento :)'
-        elsif self.rating < 4.5
+        elsif @rating < 4.5
             return 'Vai sem medo de ser feliz!!'
         else
-            return 'Quem é outback comparado a esse RU hoje?!'            
+            return 'Quem é outback comparado a esse RU hoje?!'
         end
     end
 
-
+    # Retorna a nota do cardápio de acordo com o nome do campus e refeição
     def self.get_rating(meal,campus)
         return if campus.nil? || campus.empty?
 
         @param_meal = meal
         @param_campus = campus
-        list = Rating.where('meal LIKE ? AND campus_name LIKE ?', "%#{@param_meal}%","%#{@param_campus}%")
+        list = Rating.where('meal LIKE ? AND campus_name LIKE ?',"%#{@param_meal}%","%#{@param_campus}%")
         offset = rand(list.count)
         list.offset(offset).first
     end
--- app/models/ru.rb
+++ app/models/ru.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Ru < ApplicationRecord
+
+  # Retorna o horário de funcionamento de acordo com o nome do campus
   def self.search_working_hour(search)
     return if search.nil? || search.empty?
 
