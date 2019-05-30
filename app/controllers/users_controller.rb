d98115a26986dcb3b82210e848bebd0af26825ce
ACSouzaJr <junioracs2000@hotmail.com>
2019-05-30 16:42:19 -0300

    feat: add destroy to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'usuario apagado com sucesso' }
       format.json { head :no_content }
     end
   end
