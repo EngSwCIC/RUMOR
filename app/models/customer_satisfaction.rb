52706ea730cfa5feaf621198064803cb5c9c9c5e
caeira <caioitallo.souzamendes@gmail.com>
2019-06-12 01:27:20 -0300

    refactor:new customer satisfaction

--- app/models/customer_satisfaction.rb
+++ app/models/customer_satisfaction.rb
@@ -3,10 +3,10 @@ class CustomerSatisfaction < ApplicationRecord
   #o grupo do usuario tem que ser  um numero entre 1 e 3 o rating tem que ser entre 1 e 5
   #todos os campos precisam ser preenchidos
   
-  validates :user_group, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 3}
   validates :meal, presence: true
-  validates :rating, presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+  validates :rating, presence: true
   validates :like, presence: true
   validates :disgust, presence: true
   validates :suggestion, presence: true
+  
 end
