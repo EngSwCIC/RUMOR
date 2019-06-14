88f6517eecce24821c80c8b620a810c1da9de2c0
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2019-06-14 17:25:01 -0300

    refactor: now showing current week menu properly

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -10,7 +10,17 @@ class MenusController < ApplicationController
   # Método Index é chamado para mostrar todos os cardápios cadastrados
   def index
     @menus = Menu.all.sort_by {|a| a.date }
-    @week_menus = Menu.this_week.sort_by {|a| a.date }
+    all_menus = Menu.this_week.sort_by {|a| a.date }
+
+    # Parseia os menus da semana para ter o tamanho de uma semana completa (7 dias) e apresentarem falso quando não há menu
+    week_menus_parser  = Array.new(7, false)
+    (1..7).each do |day|
+      has_menu = all_menus.select { |e| e.date.strftime("%w").to_i  == day }
+      unless has_menu.empty?
+        week_menus_parser[day-1] = has_menu.first
+      end
+    end
+    @week_menus = week_menus_parser
   end
 
   # Método que é responsável por retornar um objeto
