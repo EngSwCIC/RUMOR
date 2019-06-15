041e2c3c7624ebd1d87e6f0fef0d9d33a6868cc7
ACSouzaJr <junioracs2000@hotmail.com>
2019-06-14 22:21:11 -0300

    feat: pesquisas separadas por refeicoes e testes

---
+++ app/controllers/satisfactions_dinner_controller.rb
@@ -0,0 +1,11 @@
+class SatisfactionsDinnerController < ApplicationController
+    def index
+        @customer_satisfactions = CustomerSatisfaction.all
+        respond_to do |format|
+          format.html
+          format.pdf do
+            render pdf: "relatorio"   
+          end
+        end
+      end 
+end
---
+++ spec/controllers/satisfactions_breakfast_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe SatisfactionsBreakfastController, type: :controller do
+
+end
