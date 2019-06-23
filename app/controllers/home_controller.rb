f72655479422e660f19b67adc621d8316a71b01c
Marcos Gois <goisinho_5@hotmail.com>
2019-06-23 19:06:04 -0300

    Como usuário agora consegue visualizar o cardápio do dia, caso exista.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,12 @@
+require 'date'
+
 class HomeController < ApplicationController
 
   #metodo criado apenas pra renderização da pagina inicial
   def index
     @menus = Menu.all
     @month_menu = this_month_menu(@menus).sort_by {|a| a.date }
+    @today = Date.today
   end
 
 end
