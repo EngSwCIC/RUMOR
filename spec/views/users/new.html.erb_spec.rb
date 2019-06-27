f0cede0847086328d274115c9cce8e5e3085fa84
ACSouzaJr <junioracs2000@hotmail.com>
2019-06-27 12:28:09 -0300

    test: all rspec test passing

--- spec/views/users/new.html.erb_spec.rb
+++ spec/views/users/new.html.erb_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe "users/new", type: :view do
     render
 
     assert_select "form[action=?][method=?]", users_path, "post" do
+      assert_select "input[name=?]", "user[name]"
+      assert_select "input[name=?]", "user[email]"
+      assert_select "input[name=?]", "user[password]"
+      assert_select "input[name=?]", "user[password_confirmation]"
+      assert_select "input[name=?]", "commit"
     end
   end
 end
