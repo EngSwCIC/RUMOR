c7409b6f4449cf26115d336d8cd0b4ec1eeca9bf
PedroRocha <pedguifil@gmail.com>
2019-07-10 01:33:29 -0300

    Acesso às avaliações por meio do menu

--- app/models/review_dinner.rb
+++ app/models/review_dinner.rb
@@ -1,4 +1,12 @@
 class ReviewDinner < ApplicationRecord
     belongs_to :user
     belongs_to :dinner
+
+    validates :rating, :comment, presence: true
+    validates :rating, numericality:{
+        only_integer: true,
+        greater_than_or_equal_to: 1,
+        less_than_or_equal_to: 5,
+        message: "Somente uma nota inteira entre 1 e 5"
+    }
 end
--- app/models/review_lunch.rb
+++ app/models/review_lunch.rb
@@ -2,5 +2,11 @@ class ReviewLunch < ApplicationRecord
     belongs_to :user
     belongs_to :lunch
 
-    validates :rating, presence: true
+    validates :rating, :comment, presence: true
+    validates :rating, numericality:{
+        only_integer: true,
+        greater_than_or_equal_to: 1,
+        less_than_or_equal_to: 5,
+        message: "Somente uma nota inteira entre 1 e 5"
+    }
 end
