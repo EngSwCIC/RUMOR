66509e2be4a24e8b9e097ae89eb6266996499f4b
Gabriel Vasconcelos <gcvasconcelos98@gmail.com>
2019-07-10 16:38:13 -0300

    refactor: improve menus controller flog score

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -11,17 +11,7 @@ class MenusController < ApplicationController
   # os cardápios da semana atual
   def index
     @menus = Menu.all.sort_by {|a| a.date }.reverse!
-    all_menus = Menu.this_week.sort_by {|a| a.date }
-
-    # Parseia os menus da semana para ter o tamanho de uma semana completa (7 dias) e apresentarem falso quando não há menu
-    week_menus_parser  = Array.new(7, false)
-    (1..7).each do |day|
-      has_menu = all_menus.select { |e| e.date.strftime("%w").to_i  == day }
-      unless has_menu.empty?
-        week_menus_parser[day-1] = has_menu.first
-      end
-    end
-    @week_menus = week_menus_parser
+    @week_menus = this_week_menus
   end
 
   # Método que é responsável por retornar um objeto
@@ -239,4 +229,16 @@ class MenusController < ApplicationController
   
     end
 
+    # Parseia os menus da semana para ter o tamanho de uma semana completa (7 dias) e apresentarem falso quando não há menu
+    def this_week_menus
+      all_menus = Menu.this_week.sort_by {|a| a.date }
+      week_menus_parser  = Array.new(7, false)
+      (1..7).each do |day|
+        has_menu = all_menus.select { |e| e.date.strftime("%w").to_i  == day }
+        unless has_menu.empty?
+          week_menus_parser[day-1] = has_menu.first
+        end
+      end
+      week_menus_parser
+    end
 end
